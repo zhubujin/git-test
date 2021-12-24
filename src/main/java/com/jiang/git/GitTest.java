31d092c484fde0c074f3ced193d394afdf23975c
烛不尽 <94550768+zhubujin@users.noreply.github.com>
2021-12-24 20:43:13 +0800

    pull commit

--- src/main/java/com/jiang/git/GitTest.java
+++ src/main/java/com/jiang/git/GitTest.java
@@ -9,5 +9,8 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hot-fix test");
         System.out.println("hot-fix test22");
+        System.out.println("pull test22");
+        System.out.println("hot-fix test22");
+        System.out.println("hot-fix test22");
     }
 }
